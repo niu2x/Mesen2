f845a448f3460878c720dd3d49841c3c1e94407f
niu2x <niu2x@icloud.com>
2025-06-29 21:40:24 +0800

    DesktopApp

--- DesktopApp/src/main.cpp
+++ DesktopApp/src/main.cpp
@@ -1,11 +1,19 @@
 #include <QApplication>
+#include <QDesktopWidget>
+#include <QStyle>
 
 #include "MainWindow.h"
+
+static void center_window(QWidget* window, QApplication* app) {
+    window->setGeometry(QStyle::alignedRect(
+        Qt::LeftToRight, Qt::AlignCenter, window->size(), app->desktop()->availableGeometry()));
+}
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     MainWindow main_window;
+    center_window(&main_window, &app);
 
     main_window.show();
 
