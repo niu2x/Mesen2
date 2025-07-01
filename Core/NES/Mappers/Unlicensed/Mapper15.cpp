5cc1ce44ae960276fb4c6f4e5b97805db2a6fe85
niu2x <niu2x@icloud.com>
2025-07-02 01:05:53 +0800

    fix mapper15

--- Core/NES/Mappers/Unlicensed/Mapper15.cpp
+++ Core/NES/Mappers/Unlicensed/Mapper15.cpp
@@ -3,7 +3,8 @@
 void Mapper15::InitMapper(RomData& romData) {
     // To run these mapper-hacked games, emulators must not enforce CHR-RAM
     // write-protection even though an actual K-1029 PCB would
-    if (romData.Info.DatabaseInfo.Crc == 0xB0A706D9) {
+    // 
+    if (romData.Info.DatabaseInfo.Crc == 0xB051C67B) {
         enable_chr_ram_write_protection_ = false;
     }
 }
